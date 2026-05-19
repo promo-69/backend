4e81a6ef8235beee8bf456f2bfcff0d64e13f5dc
sansalmao <sansalmao@gmail.com>
2026-05-19 16:28:34 -0400

    fix: cambio de put a patch en la ruta uptade customer

--- src/modules/customers/_.route.ts
+++ src/modules/customers/_.route.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', verifySession, verifyPermission('CRUD:READ:CUSTOMERS'), customersController.findAll);
 router.post('/', verifySession, verifyPermission('CRUD:CREATE:CUSTOMERS'), customersController.create);
 router.get('/:id', verifySession, verifyPermission('CRUD:READ:CUSTOMERS'), customersController.findById);
-router.put('/:id', verifySession, verifyPermission('CRUD:UPDATE:CUSTOMERS'), customersController.update);
+router.patch('/:id', verifySession, verifyPermission('CRUD:UPDATE:CUSTOMERS'), customersController.update);
 router.patch(
     '/:id/loyalty-points',
     verifySession,
