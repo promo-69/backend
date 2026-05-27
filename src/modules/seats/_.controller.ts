c1a9a9cc4229aa429195581e537aa8dd5f527ab1
alirio <pastoralirio6589@gmail.com>
2026-05-27 08:36:32 -0400

    fix: corregido error al actualizar asiento

--- src/modules/seats/_.controller.ts
+++ src/modules/seats/_.controller.ts
@@ -2,31 +2,31 @@ import { ControllerBase } from '@bases/controller.base.js';
 import SeatsService from './_.service.js';
 
 class SeatsController extends ControllerBase {
-    constructor() {
-        super();
-    }
+	constructor() {
+		super();
+	}
 
-    // GET /api/v1/seats/:id
-    async findById() {
-        const { id } = this.getParams();
-        const data = await SeatsService.findById(Number(id));
-        return this.success(data, 'Asiento obtenido exitosamente');
-    }
+	// GET /api/v1/seats/:id
+	async findById() {
+		const { id } = this.getParams();
+		const data = await SeatsService.findById(Number(id));
+		return this.success(data, 'Asiento obtenido exitosamente');
+	}
 
-    // PATCH /api/v1/seats/:id
-    async update() {
-        const { id } = this.getParams();
-        const body = this.getBody();
-        await SeatsService.updateSeat(Number(id), body);
-        return this.success(null, 'Asiento actualizado correctamente');
-    }
+	// PATCH /api/v1/seats/:id
+	async update() {
+		const { id } = this.getParams();
+		const body = this.getBody();
+		const updatedSeat = await SeatsService.updateSeat(Number(id), body);
+		return this.success(updatedSeat, 'Asiento actualizado correctamente');
+	}
 
-    // DELETE /api/v1/seats/:id
-    async remove() {
-        const { id } = this.getParams();
-        await SeatsService.deleteSeat(Number(id));
-        return this.success(null, 'Asiento eliminado de la sala');
-    }
+	// DELETE /api/v1/seats/:id
+	async remove() {
+		const { id } = this.getParams();
+		await SeatsService.deleteSeat(Number(id));
+		return this.success(null, 'Asiento eliminado de la sala');
+	}
 }
 
 export default new SeatsController();
