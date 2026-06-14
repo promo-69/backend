5ae3eaecdc8251442f4dca22cb41b2f376308223
alirio <pastoralirio6589@gmail.com>
2026-06-14 15:55:31 -0400

    fix: corregido la semilla de invoice-sequences

--- src/migrate/seeders/20260614183541-invoice-sequences.js
+++ src/migrate/seeders/20260614183541-invoice-sequences.js
@@ -7,49 +7,41 @@ module.exports = {
 			'invoice_sequences',
 			[
 				{
-					id: 1,
 					cinema: 1,
 					prefix: 'SCC',
 					current_value: 1,
 				},
 				{
-					id: 2,
 					cinema: 2,
 					prefix: 'CPL',
 					current_value: 1,
 				},
 				{
-					id: 3,
 					cinema: 3,
 					prefix: 'SCO',
 					current_value: 1,
 				},
 				{
-					id: 4,
 					cinema: 4,
 					prefix: 'SCA',
 					current_value: 1,
 				},
 				{
-					id: 5,
 					cinema: 5,
 					prefix: 'SSE',
 					current_value: 1,
 				},
 				{
-					id: 6,
 					cinema: 6,
 					prefix: 'CPV',
 					current_value: 1,
 				},
 				{
-					id: 7,
 					cinema: 7,
 					prefix: 'PSU',
 					current_value: 1,
 				},
 				{
-					id: 8,
 					cinema: 8,
 					prefix: 'SP2',
 					current_value: 1,
