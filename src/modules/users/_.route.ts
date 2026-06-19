52dfe860258c7f3850fd958ee5cc609f12d9663e
bilboJB <computersjavier@gmail.com>
2026-06-18 20:56:58 -0400

    me falto volar la ruta vieja

--- src/modules/users/_.route.ts
+++ src/modules/users/_.route.ts
@@ -7,7 +7,6 @@ const router = Router();
 // --- Perfil del Usuario Logueado)
 router.get('/me', verifySession, usersController.getMyProfile);
 router.patch('/me/profile', verifySession, usersController.updateMyProfile);
-router.patch('/me/security', verifySession, usersController.updateMySecurity);
 router.post('/me/security/verify', verifySession, usersController.verifySecurity);
 router.post('/me/security/change', verifySession, usersController.changePasswordWithToken);
 router.get('/me/orders', verifySession, usersController.getMyOrders);
